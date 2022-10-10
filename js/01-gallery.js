739f6081e8784c74106307b06dd45deb388a87c9
MaksStetsenko <106399273+MaksStetsenko@users.noreply.github.com>
2022-10-10 19:10:13 +0300

    ver 1.0

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+//==================================================
+
+const markup = galleryItems
+  .map(
+    (galleryItems) =>
+      `
+      <div class="gallery__item">
+        <a class="gallery__link">
+            <img 
+              class="gallery__image"
+              src="${galleryItems.preview}" 
+              data-source="${galleryItems.original}"
+              alt="${galleryItems.description}"
+            >
+        </a>
+      </div>
+      `
+  )
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", markup);
+
+//==================================================
+
+function selectImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.getAttribute("data-source")}" width="1280">`);
+
+  instance.show();
+
+  window.addEventListener("keydown", onEsckeyPress);
+  function onEsckeyPress(event) {
+    const escBtn = "Escape";
+    const isPressEscKey = event.code === escBtn;
+
+    if (isPressEscKey) {
+      instance.close();
+    }
+  }
+}
+
+list.addEventListener("click", selectImg);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const list = document.querySelector(".gallery");
+
 console.log(galleryItems);
+
+const markup = galleryItems
+  .map(
+    (galleryItems) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItems.original}">
+                <img  
+                    class="gallery__image"
+                    src="${galleryItems.preview}" 
+                    alt="${galleryItems.description}"
+                >
+        </a>
+        </li>
+      `
+  )
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", markup);
+
+//=========================================================
+
+function selectImg(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  window.addEventListener("keydown", onEsckeyPress);
+  function onEsckeyPress(event) {
+    const escBtn = "Escape";
+    const isPressEscKey = event.code === escBtn;
+
+    if (isPressEscKey) {
+      instance.close();
+    }
+  }
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+});
+gallery.on("show.simplelightbox", function () {});
